c149635c43b15c7a39badd3f244a4562c9e9f4b3
Cong Nguyen <cnguyen1209@unm.edu>
2019-08-26 12:09:47 -0600

    Assignment 1

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(){
+  cout << "Hello World!" << endl;
+  cout << "My name is Cong Nguyen" << endl;
+  cout << "I am from Vietnam" << endl;
+  cout << "I hope I will improve my C++ skills after this class" << endl;
+  cout << "I want to go around a world and take many landscape photos" << endl;
+  return 0;
+}
---
+++ hello.md
@@ -0,0 +1,5 @@
+Hello Worl!d
+My name is Cong Nguyen
+I am from Vietnam
+I hope I will improve my C++ skills after this class
+I want to go around a world and take many landscape photos
