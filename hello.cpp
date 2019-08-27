bcd6b08bbbd498e855effdc229c99b64f1e19d54
Cong Nguyen <cnguyen1209@unm.edu>
2019-08-27 15:51:45 -0600

    Assignment 1

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 main(){
-  cout << "Hello World!" << endl;
-  cout << "My name is Cong Nguyen" << endl;
-  cout << "I am from Vietnam" << endl;
-  cout << "I hope I will improve my C++ skills after this class" << endl;
-  cout << "I want to go around a world and take many landscape photos" << endl;
+  std::cout << "#Hello! Here is some thing about me" << std::endl << std::endl;
+  std::cout << "My name is *Cong Nguyen*." <<std::endl;
+  std::cout << "I am from Vietnam." << std::endl;
+  std::cout << "I hope I will improve my C++ skills after this class." << std::endl;
+  std::cout << "I want to go around a world and take many landscape photos." << std::endl << std::endl;
+  std::cout << "###This is my photo" << std::endl << std::endl;
+  std::cout << "I took this picture on July 4th." << std::endl;
+  std::cout << "This is the first time I took photos of fireworks." << std::endl;
+  std::cout << "It was a great experience." << std::endl;
+  std::cout << "I used a photography technical called long exposure" << std::endl;
   return 0;
 }
