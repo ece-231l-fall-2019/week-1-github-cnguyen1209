73415b0ed158541551bd4869bec7add81c16bedd
Cong Nguyen <cnguyen1209@unm.edu>
2019-08-27 15:54:03 -0600

    Assignment 1

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 main(){
-  std::cout << "#Hello! Here is some thing about me" << std::endl << std::endl;
+  std::cout << "# Hello! Here is some thing about me" << std::endl << std::endl;
   std::cout << "My name is *Cong Nguyen*." <<std::endl;
   std::cout << "I am from Vietnam." << std::endl;
   std::cout << "I hope I will improve my C++ skills after this class." << std::endl;
   std::cout << "I want to go around a world and take many landscape photos." << std::endl << std::endl;
-  std::cout << "###This is my photo" << std::endl << std::endl;
+  std::cout << "### This is my photo" << std::endl << std::endl;
   std::cout << "I took this picture on July 4th." << std::endl;
   std::cout << "This is the first time I took photos of fireworks." << std::endl;
   std::cout << "It was a great experience." << std::endl;
