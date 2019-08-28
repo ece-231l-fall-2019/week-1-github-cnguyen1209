31f0383458caa5bef03d40aeea7af96057f95850
Cong Nguyen <cnguyen1209@unm.edu>
2019-08-28 12:18:12 -0600

    Assignment 1

--- hello.cpp
+++ hello.cpp
@@ -7,11 +7,11 @@ main(){
   std::cout << "I want to go around a world and take many landscape photos." << std::endl;
   std::cout << "Here is my profile link: " << std::endl;
   std::cout << "[Github](https://github.com/cnguyen1209)" << std::endl << std::endl;
-  std::cout << "### This is my photo :sparkles:	" << std::endl << std::endl;
+  std::cout << "### This is my photo :sparkles:	:sparkles:" << std::endl << std::endl;
   std::cout << "![Image of fireworks](https://github.com/ece-231l-fall-2019/week-1-github-cnguyen1209/blob/master/fireworks.jpg)" << std::endl << std::endl;
   std::cout << "I took this picture on *July 4th*." << std::endl;
   std::cout << "This is the first time I took photos of fireworks." << std::endl;
   std::cout << "It was a great experience." << std::endl;
-  std::cout << "I used a photography technique called ***long exposure*** " << std::endl;
+  std::cout << "I used a photography technique called ***long exposure***. " << std::endl;
   return 0;
 }
--- hello.md
+++ hello.md
@@ -7,11 +7,11 @@ I want to go around a world and take many landscape photos.
 Here is my profile link: 
 [Github](https://github.com/cnguyen1209)
 
-### This is my photo :sparkles:	
+### This is my photo :sparkles:	:sparkles:
 
 ![Image of fireworks](https://github.com/ece-231l-fall-2019/week-1-github-cnguyen1209/blob/master/fireworks.jpg)
 
 I took this picture on *July 4th*.
 This is the first time I took photos of fireworks.
 It was a great experience.
-I used a photography technique called ***long exposure*** 
+I used a photography technique called ***long exposure***. 
