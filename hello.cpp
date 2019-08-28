de4bcaf243ec3d5b12350f8ee792f082f7efa70c
Cong Nguyen <cnguyen1209@unm.edu>
2019-08-27 20:42:07 -0600

    Assignment 1

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 main(){
-  std::cout << "# Hello! Here is some thing about me" << std::endl << std::endl;
+  std::cout << "### Hello! Here is some thing about me" << std::endl << std::endl;
   std::cout << "My name is *Cong Nguyen*." <<std::endl;
-  std::cout << "I am from Vietnam." << std::endl;
+  std::cout << "I am from **Vietnam**." << std::endl;
   std::cout << "I hope I will improve my C++ skills after this class." << std::endl;
   std::cout << "I want to go around a world and take many landscape photos." << std::endl << std::endl;
   std::cout << "### This is my photo" << std::endl << std::endl;
-  std::cout << "I took this picture on July 4th." << std::endl;
+  std::cout << "I took this picture on *July 4th*." << std::endl;
   std::cout << "This is the first time I took photos of fireworks." << std::endl;
   std::cout << "It was a great experience." << std::endl;
-  std::cout << "I used a photography technical called long exposure" << std::endl;
+  std::cout << "I used a photography technical called ***long exposure*** time" << std::endl;
   return 0;
 }
--- hello.md
+++ hello.md
@@ -1,13 +1,13 @@
-# Hello! Here is some thing about me
+### Hello! Here is some thing about me
 
 My name is *Cong Nguyen*.
-I am from Vietnam.
+I am from **Vietnam**.
 I hope I will improve my C++ skills after this class.
 I want to go around a world and take many landscape photos.
 
 ### This is my photo
 
-I took this picture on July 4th.
+I took this picture on *July 4th*.
 This is the first time I took photos of fireworks.
 It was a great experience.
-I used a photography technical called long exposure
+I used a photography technical called ***long exposure*** time
