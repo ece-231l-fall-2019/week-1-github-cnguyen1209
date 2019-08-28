1fe10642e000e1a350e88fddf7c398aad66676b5
Cong Nguyen <cnguyen1209@unm.edu>
2019-08-28 12:03:53 -0600

    Assignment 1

--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,7 @@ main(){
   std::cout << "I hope I will improve my C++ skills after this class." << std::endl;
   std::cout << "I want to go around a world and take many landscape photos." << std::endl << std::endl;
   std::cout << "### This is my photo" << std::endl << std::endl;
+  std::cout << "[Image of fireworks](https://github.com/ece-231l-fall-2019/week-1-github-cnguyen1209/blob/master/fireworks.jpg)" << std::endl << std::endl;
   std::cout << "I took this picture on *July 4th*." << std::endl;
   std::cout << "This is the first time I took photos of fireworks." << std::endl;
   std::cout << "It was a great experience." << std::endl;
--- hello.md
+++ hello.md
@@ -7,6 +7,8 @@ I want to go around a world and take many landscape photos.
 
 ### This is my photo
 
+[Image of fireworks](https://github.com/ece-231l-fall-2019/week-1-github-cnguyen1209/blob/master/fireworks.jpg)
+
 I took this picture on *July 4th*.
 This is the first time I took photos of fireworks.
 It was a great experience.
