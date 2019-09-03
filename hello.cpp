0f1b532c25658047c1099e7e497ce4383682543f
Cong Nguyen <cnguyen1209@unm.edu>
2019-09-03 13:49:00 -0600

    Assignment 1

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 main(){
+  std::cout << "## Cong's Profile" << std::endl << std::endl;
   std::cout << "### Hello! Here is some thing about me" << std::endl << std::endl;
   std::cout << "My name is *Cong Nguyen*." <<std::endl;
   std::cout << "I am from **Vietnam**." << std::endl;
+  std::cout << "I have lived in Albuquerque for 3 years" << std::endl;
+  std::cout << "My major is Computer Engineering" << std:: endl;
   std::cout << "I hope I will improve my C++ skills after this class." << std::endl;
   std::cout << "I want to go around a world and take many landscape photos." << std::endl;
   std::cout << "Here is my profile link: " << std::endl;
   std::cout << "[Github](https://github.com/cnguyen1209)" << std::endl << std::endl;
   std::cout << "### This is my photo :sparkles:	:sparkles:" << std::endl << std::endl;
   std::cout << "![Image of fireworks](https://github.com/ece-231l-fall-2019/week-1-github-cnguyen1209/blob/master/fireworks.jpg)" << std::endl << std::endl;
-  std::cout << "I took this picture on *July 4th*." << std::endl;
+  std::cout << "I took this picture on *July 4th, 2019*." << std::endl;
   std::cout << "This is the first time I took photos of fireworks." << std::endl;
   std::cout << "It was a great experience." << std::endl;
   std::cout << "I used a photography technique called ***long exposure***. " << std::endl;
+  std::cout << "It took me 20s to take this photo." << std::endl;
   return 0;
 }
--- hello.md
+++ hello.md
@@ -1,7 +1,11 @@
+## Cong's Profile
+
 ### Hello! Here is some thing about me
 
 My name is *Cong Nguyen*.
 I am from **Vietnam**.
+I have lived in Albuquerque for 3 years
+My major is Computer Engineering
 I hope I will improve my C++ skills after this class.
 I want to go around a world and take many landscape photos.
 Here is my profile link: 
@@ -11,7 +15,8 @@ Here is my profile link:
 
 ![Image of fireworks](https://github.com/ece-231l-fall-2019/week-1-github-cnguyen1209/blob/master/fireworks.jpg)
 
-I took this picture on *July 4th*.
+I took this picture on *July 4th, 2019*.
 This is the first time I took photos of fireworks.
 It was a great experience.
 I used a photography technique called ***long exposure***. 
+It took me 20s to take this photo.
